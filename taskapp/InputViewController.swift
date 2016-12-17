2e962c34cb4ee2600daad666a3604eb25b1f460c
kenren <san3ichi_mile@gmail.com>
2016-12-17 21:24:43 +0900

    レッスン6.10まで実装

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,66 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by 佐々井健吾 on 2016/12/13.
+//  Copyright © 2016年 kenren. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class InputViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    let realm = try! Realm()
+    var task:Task!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 背景をタップしたらdismissKeyboardメソッドを呼ぶように設定する
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tapGesture)
+        
+        titleTextField.text = task.title
+        contentsTextView.text = task.contents
+        datePicker.date = task.date as Date
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //遷移する際に画面が非表示になるとき呼ばれるメソッド
+    override func viewWillDisappear(_ animated: Bool) {
+        try! realm.write {
+            self.task.title = self.titleTextField.text!
+            self.task.contents = self.contentsTextView.text
+            self.task.date = self.datePicker.date as NSDate
+            self.realm.add(self.task, update: true)
+        }
+        
+        super.viewWillDisappear(animated)
+    }
+    
+    func dismissKeyboard(){
+        // キーボードを閉じる
+        view.endEditing(true)
+    }
+}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
